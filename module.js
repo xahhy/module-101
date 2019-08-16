676bd74956b2849388937e95c3664c53db633b3a
hoyhe <hoyhe@thoughtworks.com>
2019-08-17 00:22:06 +0800

    step11: require multiple times problem

--- module.js
+++ module.js
@@ -6,35 +6,26 @@ class Module {
 }
 
 // src/math.js
-// 1. Compile (wrap with a function)
 function math(module, require) {
   const square = (x) => x * 2;
+  const timestamp = Date.now();
+  console.log(`load ${module.id}`);
   module.exports = {
     square,
+    timestamp,
   };
 }
-// 2. Create Module
-const mathModule = new Module('math', {});
-// 3. Load Module (Run the function)
-math(mathModule);
-console.log(mathModule);
 
 // src/index.js
-// 4. Compile
 function index(module, require) {
   const math = require('./math');
 
   console.log(math.square(2));
-}
-// 5. Create Module
-const indexModule = new Module('index', {});
-// 6. Require Another Module
-const __require__ = (id) => {
-  return mathModule.exports;
-};
-// 7. Load Modules
-index(indexModule, __require__);
+  console.log(math.timestamp);
 
+  const __math__ = require('./math');
+  console.log(__math__.timestamp);
+}
 /**
  * Refactor Version
  */

10daa827bb5db105f4308fe7bbc9d4ada9b2087d
hoyhe <hoyhe@thoughtworks.com>
2019-08-16 23:54:42 +0800

    step6: create module for index and math

--- module.js
+++ module.js
@@ -4,3 +4,20 @@ class Module {
     this.exports = exports;
   }
 }
+
+// src/math.js
+function math(module, require) {
+  const square = (x) => x * 2;
+  module.exports = {
+    square,
+  };
+}
+const mathModule = new Module('math', {});
+
+// src/index.js
+function index(module, require) {
+  const math = require('./math');
+
+  console.log(math.square(2));
+}
+const indexModule = new Module('index', {});

97a36f91eeec9986ba050b88dfe21e53c38e6fdd
hoyhe <hoyhe@thoughtworks.com>
2019-08-17 00:07:48 +0800

    step9: add comments on each step

--- module.js
+++ module.js
@@ -6,24 +6,31 @@ class Module {
 }
 
 // src/math.js
+// 1. Compile (wrap with a function)
 function math(module, require) {
   const square = (x) => x * 2;
   module.exports = {
     square,
   };
 }
+// 2. Create Module
 const mathModule = new Module('math', {});
+// 3. Load Module (Run the function)
 math(mathModule);
 console.log(mathModule);
 
 // src/index.js
+// 4. Compile
 function index(module, require) {
   const math = require('./math');
 
   console.log(math.square(2));
 }
+// 5. Load Module
 const indexModule = new Module('index', {});
+// 6. Require Another Module
 const __require__ = (id) => {
   return mathModule.exports;
 };
+// 7. Load Modules
 index(indexModule, __require__);
